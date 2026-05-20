43c13ed884278a3b1fb857a512b1493e6b32e142
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2026-05-20 03:54:38 -0500

    Fix quoting for file path in LoadPS1Functions

--- LoadFunctions.sh
+++ LoadFunctions.sh
@@ -1,8 +1,11 @@
+#!/bin/env bash
+# shellcheck source=/dev/null
+
 function LoadPS1Functions() {
         PS1ConfigTool_FunctionsFile="./PS1ConfigTool.bfunc"
-        if [ -f $PS1ConfigTool_FunctionsFile ]; then
+        if [ -f "$PS1ConfigTool_FunctionsFile" ]; then
                 echo -n "Sourcing Functions..."
-                source $PS1ConfigTool_FunctionsFile
+                source "$PS1ConfigTool_FunctionsFile"
 ###             source ./PS1ConfigTool.bfunc
                 echo "Functions Loaded."
                 echo " "
